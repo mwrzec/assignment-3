253a72790706bb7f00e801ea19b0a9c8e3f388e6
mwrzec <marcel.wrzecionek@gmail.com>
2023-11-18 18:30:30 -0500

    created account menu

--- application.py
+++ application.py
@@ -30,4 +30,40 @@ class Application:
                 break
             else:
                 print("Invalid Selection. Try again.")
-                
+    def show_acc_menu(self,account):
+        while True:
+            print("\nAccount Menu:")
+            print("1. Check Balance")
+            print("2. Deposit Money")
+            print("3. Withdrawl Money")
+            print("4. Exit Account")
+
+            select = input("Enter your selection: ")
+
+            if select == "1":
+                print(f"Balance: {account.balance()}")
+
+            elif select == "2":
+                amount = float(input("Enter the amount you want to deposit: "))
+                account.depo(amount)
+                print("Deposit sucessful!")
+
+            elif select == "3":
+                amount = float("Enter the amount you want to withdrawl: ")
+                if account.withdrawl(amount):
+                    print("Withdrawl sucessful!")
+
+                else:
+                    print("Insufficient funds!")
+
+            elif select == "4":
+                print("Exiting Account Menu")
+                break
+            else:
+                print("Invalid Selection. Try again.")
+
+
+
+
+
+
