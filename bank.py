6663384f3416c76c2eb19f33d4629f90933d3d3a
mwrzec <marcel.wrzecionek@gmail.com>
2023-11-18 18:54:35 -0500

    created function to make default accounts

--- bank.py
+++ bank.py
@@ -16,4 +16,8 @@ class Bank:
         return ''.join(str(random.randint(0,9)) for _ in range(random.randint(8,12)))
     
     def create_def_acc(self):
-        
+        self.add_acc(ChequingAcc(self.create_acc_num(),5000, 1000))
+        self.add_acc(ChequingAcc(self.create_acc_num(),7000, 1500))
+        self.add_acc(SavingsAcc(self.create_acc_num(),10000, 5000))
+        self.add_acc(SavingsAcc(self.create_acc_num(),15000, 6000))
+        self.add_acc(SavingsAcc(self.create_acc_num(),8000, 4000))
