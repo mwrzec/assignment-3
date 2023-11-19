be63cb18220832c2687cb42921a4c3ca820a64a6
mwrzec <marcel.wrzecionek@gmail.com>
2023-11-18 19:04:02 -0500

    created account class and its functions

---
+++ account.py
@@ -0,0 +1,25 @@
+class Account:
+    def __init__(self, acc_num, balance):
+        self.acc_num = acc_num
+        self.balance = balance
+
+    def get_bal(self):
+        return self.balance
+    
+    def deposit(self,amount):
+        if amount > 0:
+            self.balance += amount
+            return True
+        else:
+            print("Invalid deposit amount!")
+            return False
+        
+    def withdrawl(self, amount):
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            return True
+        else:
+            print("Invalid withdrawl amount!")
+            return False
+        
+    
